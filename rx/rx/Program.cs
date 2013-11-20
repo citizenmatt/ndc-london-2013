94e524093131c10708acd730ae3f29bfa3921ef6
Matt Ellis <m.t.ellis@gmail.com>
2013-11-20 21:23:10 +0000

    Console app to download string via Task

---
+++ rx/rx/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wc = new WebClient();
+            var task = wc.DownloadStringTaskAsync("http://www.google.com/robots.txt");
+            task.ContinueWith(t => Console.WriteLine(t.Result));
+
+            // Wait for the async call
+            Console.ReadLine();
+        }
+    }
+}
